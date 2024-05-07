a1f6b81497da9460d6268464aa5c374a8af6752a
Candice Milburn <candicemilburn@hotmail.com>
2024-05-07 14:02:00 -0500

    completed assignments

---
+++ scripts/numbers.js
@@ -0,0 +1,17 @@
+"use strict"
+console.log("hello from numbers.js")
+
+let num= 4
+
+num = num+15
+
+console.log(num);
+
+num = num-8
+console.log(num);
+
+numAvg = 340/5
+console.log(numAvg);
+
+let theNumber = 14;
+console.log(++theNumber)
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,2 +1,30 @@
 "use strict"
-console.log("hello from Candice")
+console.log("hello from variables.js")
+
+let price = 25
+
+
+var product = "boots"
+
+const color = "green"
+
+
+console.log("The price is $" + price);
+
+console.log(product);
+
+console.log(color);
+
+price = 30
+product = "shirt"
+console.log(price)
+console.log(product)
+
+color = "red"
+console.log(color)
+
+//If you try to change the const it will return an
+// error because constants do not change
+
+//var doesn't return all of the errors it can cause so 
+//its best practice to use the let and const
